4c094ab9e5d6a02dd05fc2315adfea7fd8b5f430
Olle <ollenglund@outlook.com>
2026-03-27 01:04:24 +0100

    comment change and add pa5_to_af/gpio

--- led.c
+++ led.c
@@ -5,9 +5,10 @@
 void led_init(void) {
     // PA 5 
     RCC_AHB1ENR |= (1 << 0); // RCC AHB1 peripheral clock enable register, Bit 0 GPIOAEN: IO port A clock enable
-    GPIOA_MODER |= (1 << 10); // bit 10 = 1 
-    GPIOA_MODER &= ~(1 << 11); // bit 11 = 0         (01 = General purpose output mode)
-    GPIOA_OTYPER &= ~(1 << 5); // GPIO port output type register.  PA5, Output open-drain (0)
+
+    GPIOA_MODER &= ~(0b11 << 10);
+    GPIOA_MODER |= (0b01 << 10); // general purpose output 
+    GPIOA_OTYPER &= ~(1 << 5); // push-pull
 }
 
 void led_on(void) {
@@ -16,4 +17,17 @@ void led_on(void) {
 
 void led_off(void) {
     GPIOA_BSRR |= (1 << (16 + 5));
+}
+
+void pa5_to_af(void) {
+    GPIOA_MODER &= ~(0b11 << 10);
+    GPIOA_MODER |= (0b10 << 10); // alternate function
+
+    GPIOA_AFRL  &= ~(0xF << 20);   // clear AF bits for PA5
+    GPIOA_AFRL  |=  (0x1 << 20);   // AF1 = TIM2
+}
+
+void pa5_to_gpio(void) {
+    GPIOA_MODER &= ~(0b11 << 10);
+    GPIOA_MODER |= (0b01 << 10); // output
 }
--- led.h
+++ led.h
@@ -5,3 +5,7 @@ void led_init(void);
 void led_on(void);
 
 void led_off(void);
+
+void pa5_to_af(void);
+
+void pa5_to_gpio(void);
