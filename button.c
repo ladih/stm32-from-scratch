abf325d86dc0a5bf2fb0c2bf1df5bfac368d1a0c
ladih <138324910+ladih@users.noreply.github.com>
2026-03-28 20:36:14 +0100

    Comments change in button.c

--- button.c
+++ button.c
@@ -9,15 +9,13 @@ void button_init() {
     // "B1 USER: the user button is connected to the I/O PC13 (pin 2) of the STM32 microcontroller."
 
     RCC_AHB1ENR |= (1 << 2); // enable clock for GPIOC
-    // GPIOC_MODER |= (0 << 26);    input config for PC 13
-    // GPIOC_MODER |= (0 << 27);
-
-    
-    RCC_APB2ENR |= (1 << 14); // SYSCFGEN: System configuration controller clock enable
-    SYSCFG_EXTICR4 |= (1 << 5); // map EXTI13 to port C
-
+    GPIOC_MODER &= ~(0b11 << 26); // 00 = input  (PC 13)
+    RCC_APB2ENR |= (1 << 14); // 1: System configuration controller clock enabled
+    SYSCFG_EXTICR4 |= (0b0010 << 4); // map EXTI13 to port C  (0010 = C)
     EXTI_IMR |= (1 << 13);   // unmask interrupt on line 13
-    EXTI_FTSR |= (1 << 13);  // falling edge (button press if pull-up)
+
+    EXTI_FTSR |= (1 << 13);  // enable falling edge (button press if pull-up)
+    // EXTI->RTSR |=  (1 << 13); // enable rising edge (trigger on release)
 
     NVIC_ISER1 |= (1 << 8);     // Enable NVIC for EXTI_15 to EXTI_10.
 }
@@ -26,6 +24,6 @@ void button_init() {
 void EXTI15_10_IRQHandler(void) {
     if (EXTI_PR & (1 << 13)) {   // check pending bit
         button_pressed = 1;
-        EXTI_PR |= (1 << 13);    // clear by writing 1
+        EXTI_PR |= (1 << 13);    // clear by writing 1    
     }
 }
--- button.h
+++ button.h
@@ -3,4 +3,5 @@
 extern volatile int button_pressed;
 
 void EXTI15_10_IRQHandler(void);
+
 void button_init(void);
