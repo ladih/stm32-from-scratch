17c411916e8e7e9df27ebad22d40fa8382e6910c
ladih <138324910+ladih@users.noreply.github.com>
2026-04-17 23:43:57 +0200

    change 0x7 to 0b111

--- systick.c
+++ systick.c
@@ -11,5 +11,5 @@ unsigned int get_ticks(void) { return tick_count; }
 void SysTick_Init(void) {
     SYST_RVR = 16000 - 1; // 16 Mhz -> 16000 cycles = 1ms
     SYST_CVR = 0;     // Clear current value
-    SYST_CSR = 0x7;  // use processor clock, enable systick exception, enable counter
+    SYST_CSR = 0b111;  // use processor clock, enable systick exception, enable counter
 }
